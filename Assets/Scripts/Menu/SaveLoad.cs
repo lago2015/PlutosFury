1fc0d34a69bde00dae049c7b485ae567178f9bf6
AnagramMC <hisham.ata@hotmail.com>
2017-07-25 20:39:59 -0400

    Seralizer and SaveLoad class in

---
+++ Assets/Scripts/Menu/SaveLoad.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class SaveLoad : MonoBehaviour {
+
+    public Movement plutoClassSave;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void LoadGame()
+    {
+        Movement newSimpleClass = Serializer.Load<Movement>("save.txt");
+        Debug.Log(newSimpleClass);
+    }
+
+    public void SaveGame()
+    {
+        Serializer.Save<Movement>("save.txt", plutoClassSave);
+        Debug.Log(plutoClassSave);
+        Debug.Log("Saved");
+    }
+}
