faf351ac7da8142653642781ac1e0036a3341c88
Douglas1701 <matthew@matthewwalterdouglas.com>
2017-10-18 19:32:13 -0400

    Basic Creation of Shatter

---
+++ Assets/Scripts/AI/Shatter.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shatter : MonoBehaviour
+{
+    
+
+    private Animator anim;
+    private bool playerInRange;
+    private bool detached = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+	
+    void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("ENTER");
+
+        if (collision.tag == "Player")
+        {
+            playerInRange = true;
+
+            Debug.Log("Entered");
+
+            DetachOrRetract(true);
+        }
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        if (collision.tag == "Player")
+        {
+            playerInRange = false;
+        }
+    }
+
+    public void ActionCheck()
+    {
+        if (detached)
+        {
+            StartCoroutine(WaitForAction(3, false));
+        }
+        else
+        {
+            if (playerInRange)
+            {
+                StartCoroutine(WaitForAction(1, true));
+            }
+        }
+    }
+
+    void DetachOrRetract(bool detach)
+    {
+        if (detach)
+        {
+            anim.SetTrigger("Detach");
+            detached = true;
+        }
+        else
+        {
+            anim.SetTrigger("Retract");
+            detached = false;
+        }
+    }
+
+    IEnumerator WaitForAction(int seconds, bool detach)
+    {
+        yield return new WaitForSeconds(seconds);
+        DetachOrRetract(detach);
+    }
+}
--- Assets/Scripts/AI/ShatterCore.cs
+++ Assets/Scripts/AI/ShatterCore.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShatterCore : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Debug.Log("Destroy!");
+        }
+    }
+}
