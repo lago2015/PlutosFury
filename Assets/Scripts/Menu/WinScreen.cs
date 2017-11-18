2509d699138f41005726632fbd1851a3d0d162c4
AnagramMC <hisham.ata@hotmail.com>
2017-11-17 23:14:33 -0500

    Additon of win screen

---
+++ Assets/Scripts/Menu/WinScreen.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WinScreen : MonoBehaviour {
+
+    int AsteroidsCollected;
+    float Timer;
+    int TotalScore;
+
+    public Text timeDisplay;
+    public Text scoreDisplay;
+    public Text totalDisplay;
+
+	// Use this for initialization
+	void Start () {
+        timeDisplay.text = Timer.ToString();
+        scoreDisplay.text = AsteroidsCollected.ToString();
+
+        totalDisplay.text = TotalScore.ToString();
+	}
+
+    void Update()
+    {
+        Timer += Time.deltaTime;
+        timeDisplay.text = Timer.ToString();
+    }
+
+    public void SetAsteroidsCollected(int Temp)
+    {
+        AsteroidsCollected = Temp;
+    }
+
+    public void setTime(float Temp)
+    {
+        Timer = Temp;
+    }
+
+	// Update is called once per frame
+	
+}
