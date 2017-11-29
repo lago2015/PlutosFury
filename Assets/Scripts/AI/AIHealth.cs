06d2d02833c3671504d9d48ec1684a5cfa69d8e1
Douglas1701 <matt.w.doug@gmail.com>
2017-11-29 14:46:34 -0500

    Basic Moonball functionality in!!
    MoonBall functionality in and should work for most objects! still needs some tweaking on certain circumstances

--- Assets/Scripts/AI/AIHealth.cs
+++ Assets/Scripts/AI/AIHealth.cs
@@ -13,17 +13,14 @@ public class AIHealth : MonoBehaviour {
     public GameObject Model;
     public GameObject Model2;
     private Collider myCollider;
-    public bool ToCollidedWith=true;
+   
     public float wallBump = 20;
     private Rigidbody myBody;
     private FleeOrPursue RogueScript;
     void Awake()
     {
 
-        if(gameObject.tag=="BreakableWall")
-        {
-            ToCollidedWith = false;
-        }
+        
         myCollider = GetComponent<Collider>();
         myBody = GetComponent<Rigidbody>();
         if(Explosion&&Model)
@@ -44,13 +41,27 @@ public class AIHealth : MonoBehaviour {
         EnemyHealth--;
         if(EnemyHealth<=0)
         {
-            
+
             if (Explosion && Model)
             {
-                myCollider.enabled = false;
-                
+              // myCollider.enabled = false;
+
                 Explosion.SetActive(true);
-                Model.SetActive(false);
+                Explosion.transform.parent = null;
+
+                Destroy(Explosion, Explosion.GetComponent<ParticleSystem>().main.duration);
+
+                if (transform.parent != null)
+                {
+                    Destroy(transform.parent.gameObject);
+                }
+                else
+                {
+                    Destroy(this.gameObject);
+                }
+
+            }
+              /*  Model.SetActive(false);
                 if(Model2)
                 {
                     
@@ -62,13 +73,13 @@ public class AIHealth : MonoBehaviour {
             {
                 Destroy(transform.parent.gameObject);
             }
+            */
         }
     }
 
     void OnCollisionEnter(Collision col)
     {
-        if(ToCollidedWith)
-        {
+        
             string CurTag = col.gameObject.tag;
 
             if (CurTag == "Player")
@@ -98,14 +109,12 @@ public class AIHealth : MonoBehaviour {
 
                 myBody.AddForce(col.contacts[0].normal * wallBump, ForceMode.VelocityChange);
             }
-        }
+        
     }
 
     void OnTriggerEnter(Collider col)
     {
-        if(ToCollidedWith)
-        {
-
+      
             if (col.gameObject.tag == "Player")
             {
                 Movement playerScript = col.GetComponent<Movement>();
@@ -129,13 +138,13 @@ public class AIHealth : MonoBehaviour {
                 if (moonBall.getAttackMode())
                 {
                     IncrementDamage();
+                    moonBall.KnockBack(this.gameObject);
                 }
                 else
                 {
                     moonBall.KnockBack(this.gameObject);
                 }
-            }
-        }
+            } 
     }
 }
 
--- Assets/Scripts/EnvironmentObstacles/MoonBall.cs
+++ Assets/Scripts/EnvironmentObstacles/MoonBall.cs
@@ -5,25 +5,30 @@ public class MoonBall : MonoBehaviour
 {
     public float hitSpeed;
     public float knockbackSpeed;
+    public float idleSpeed = 10.0f;
     private Rigidbody rb;
 
     private bool attackMode = false;
+    private bool canHit = true;
 
     // Use this for initialization
 	void Start ()
     {
+        // Get rigibody component
         rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        // IF ball is in attack mode, switch it back to idle mode when velocity goes under a specific speed
 	    if(attackMode)
         {
-            if (rb.velocity.magnitude < 10)
+            if (rb.velocity.magnitude < idleSpeed)
             {
                 attackMode = false;
 
+                // code to switch sprite colour to indicate its in idle -> WILL NEED MATERIAL FOR THIS NOW
               //  transform.FindChild("Sprite").GetComponent<SpriteRenderer>().color = Color.white;
 
             }
@@ -39,8 +44,10 @@ public class MoonBall : MonoBehaviour
     {
         if (col.tag == "Player")
         {
-            if (!attackMode)
+            // check if player can hit moonball, this is use so player can not double hit the ball and velocity change because of knockback effect
+            if (canHit)
             {
+                // Get the player's direction and apply speed to launch the ball in that direction
                 Movement playerMovement = col.gameObject.GetComponent<Movement>();
                 if (playerMovement)
                 {
@@ -52,11 +59,17 @@ public class MoonBall : MonoBehaviour
 
                         rb.velocity = playerdirection * hitSpeed;
 
+                        // add rotation to the ball since it is 3D
                         rb.AddTorque(playerdirection * hitSpeed);
 
+                        // turn on attack mode so it can destroy enemies and obsticles
                         attackMode = true;
 
-                       // transform.FindChild("Sprite").GetComponent<SpriteRenderer>().color = Color.green;
+                        // This is to make sure player can not double hit ball, toggles the canHit boolean in a interval.
+                        StartCoroutine(HitBreak());
+
+                        // code to switch sprite colour to indicate its in attack -> WILL NEED MATERIAL FOR THIS NOW
+                        // transform.FindChild("Sprite").GetComponent<SpriteRenderer>().color = Color.green;
                     }
                 }
             }
@@ -64,11 +77,12 @@ public class MoonBall : MonoBehaviour
 
         if(col.tag == "Wall")
         {
+            // WORK AROUND: for wall, switch the moon ball's collider to solid to apply bounce physics on outer walls
             GetComponent<SphereCollider>().isTrigger = false;
             Debug.Log("HIT WALL");
         }
 
-       if(col.gameObject.name == "Spikes" || col.tag =="ShatterPiece" || col.tag == "LazerWall")
+       if(col.gameObject.name == "Spikes" || col.tag =="ShatterPiece" || col.gameObject.name == "LaserWall")
        {
            KnockBack(col.gameObject);
 
@@ -78,6 +92,7 @@ public class MoonBall : MonoBehaviour
 
     private void OnTriggerExit(Collider col)
     {
+        // WORK AROUND: for wall, switches back collider to trigger after exits the outer wall to apply normal collision logic
         if (col.tag == "Wall")
         {
             GetComponent<SphereCollider>().isTrigger = true;
@@ -86,7 +101,8 @@ public class MoonBall : MonoBehaviour
 
     private void OnCollisionEnter(Collision col)
     {
-        if(col.gameObject.tag == "LazerWall")
+        // Dont think this is needed at all, but will leave for now
+        if(col.gameObject.name == "LazerWall")
         {
             KnockBack(col.gameObject);
 
@@ -96,11 +112,19 @@ public class MoonBall : MonoBehaviour
 
     public void KnockBack(GameObject target)
     {
+        // Knockback for now: simply just reverses the direction of the ball and applies a certain speed
          Vector3 knockBackDirection = target.transform.position - transform.position;
-        knockBackDirection = knockBackDirection.normalized;
-        // rb.AddForce(-knockBackDirection * knockbackSpeed * 2, ForceMode.Impulse);
-
+         knockBackDirection = knockBackDirection.normalized;
+       
         rb.AddForce(-rb.velocity * 1.5f , ForceMode.Impulse);
+    }
+
+    IEnumerator HitBreak()
+    {
+        // toggles the can hit to make sure player can not double hit the ball while inside trigger collider.
+        canHit = false;
+        yield return new WaitForSeconds(0.5f);
+        canHit = true;
     }
 
 }
