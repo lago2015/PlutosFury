9d48544d384707229e7dcf040105877f8b295ab1
AnagramMC <hisham.ata@hotmail.com>
2018-02-18 23:41:13 -0500

    game over button fade.
    game over button fade in.

--- Assets/Scripts/Menu/LoseScreen.cs
+++ Assets/Scripts/Menu/LoseScreen.cs
@@ -10,24 +10,29 @@ public class LoseScreen : MonoBehaviour {
     public Button MainMenu;
 
     public Image fadeOverlay;
-    public Image gameFade;
 
     public Image plutoSprite;
 
+    public float fadeDuration = 15.0f;
     void Start()
     {
         plutoSprite.transform.position.Set(0f, -450f, 0);
+        RestartLevel.image.canvasRenderer.SetAlpha(0.0f);
+        MainMenu.image.canvasRenderer.SetAlpha(0.0f);
+        StartCoroutine(startFade());
     }
 
     public void ResetPositions()
     {
         plutoSprite.transform.position.Set(0f, -450f, 0);
+        RestartLevel.image.canvasRenderer.SetAlpha(0.0f);
+        MainMenu.image.canvasRenderer.SetAlpha(0.0f);
     }
 
 	// Update is called once per frame
 	void Update ()
     {
-	    if (plutoSprite.transform.position.y < 500)
+        if (plutoSprite.transform.position.y < 500)
         {
             plutoSprite.transform.Translate(Vector3.up * 500 * Time.deltaTime, Space.World);
         }
@@ -35,6 +40,20 @@ public class LoseScreen : MonoBehaviour {
         {
             plutoSprite.transform.position.Set(0, 500, 0);
         }
+        Debug.Log(RestartLevel.GetComponent<Image>().color.a);
 	}
 
+    void FadeIn()
+    {
+        fadeOverlay.CrossFadeAlpha(0, fadeDuration, true);
+        RestartLevel.image.CrossFadeAlpha(1, fadeDuration, true);
+        MainMenu.image.CrossFadeAlpha(1, fadeDuration, true);
+    }
+
+    private IEnumerator startFade()
+    {
+        FadeIn();
+
+        yield return new WaitForSeconds(fadeDuration);
+    }
 }
