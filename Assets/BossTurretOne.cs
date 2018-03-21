e0a5609a06cb4a2295a338bcb31f353ad4cc5c86
Douglas1701 <matt.w.doug@gmail.com>
2018-03-20 20:45:50 -0400

    Boss Rotating Turret Work

---
+++ Assets/BossTurretOne.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTurretOne : MonoBehaviour
+{
+    public float upAngle;
+    public float downAngle;
+
+    public float speed;
+
+    public bool isGoingUp = true;
+
+    Quaternion currentRot;
+
+	// Use this for initialization
+	void Start () {
+        currentRot = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(isGoingUp)
+        {
+            transform.Rotate(Vector3.forward * speed * Time.deltaTime);
+
+            float angle = transform.localEulerAngles.z;
+            angle = (angle > 180) ? angle - 360 : angle;
+
+            if (angle >= upAngle)
+            {
+                isGoingUp = false;
+            }
+
+        }
+        else
+        {
+            transform.Rotate(Vector3.back * speed * Time.deltaTime);
+
+            float angle = transform.localEulerAngles.z;
+            angle = (angle > 180) ? angle - 360 : angle;
+
+            if(angle <= downAngle)
+            {
+                isGoingUp = true;
+            }
+
+
+        }
+	}
+}
