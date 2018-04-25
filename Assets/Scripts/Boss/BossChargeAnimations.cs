87eb73cce5d87629c7c1d7d78b3b686d04d8ad2e
lago2015 <chilenoo92@gmail.com>
2018-04-24 22:27:41 -0400

    Boss Animations for Charge

---
+++ Assets/Scripts/Boss/BossChargeAnimations.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossChargeAnimations : MonoBehaviour {
+
+    private Animator animComp;
+    private int clipIndex; //0 is idle, 1-top 2-center 3-bottom lanes
+    private float curAnimationClip;
+
+    private void Awake()
+    {
+        animComp = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        RandomizeChargeLane();
+    }
+
+    void RandomizeChargeLane()
+    {
+        Debug.Log("Current Animation Clip: " + clipIndex);
+
+        clipIndex = Random.Range(1, 3);
+        animComp.SetInteger("LaneNumber", clipIndex);
+        curAnimationClip = animComp.GetCurrentAnimatorClipInfo(0).Length;
+        Debug.Log("Current Animation Clip: " + clipIndex);
+        StartCoroutine(WaitForAnimation());
+    }
+
+    IEnumerator WaitForAnimation()
+    {
+        yield return new WaitForSeconds(curAnimationClip);
+        animComp.SetInteger("LaneNumber", 0);
+        yield return new WaitForSeconds(3);
+        RandomizeChargeLane();
+    }
+
+
+}
---
+++ Assets/Scripts/Boss/BossSpawnAfterCharge.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpawnAfterCharge : MonoBehaviour {
+
+    public GameObject[] SpawnGroup;
+    private int index;
+    public GameObject spawnPoint;
+    
+
+    public void PlaceGroupInWorld()
+    {
+        index = Random.Range(0, SpawnGroup.Length - 1);
+        Instantiate(SpawnGroup[index], spawnPoint.transform.position, Quaternion.identity);
+    }
+}
