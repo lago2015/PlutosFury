ee9ac08549a6c6d2e8cf3fbe7c14e34d90b6d390
AnagramMC <hisham.ata@hotmail.com>
2018-05-20 17:05:30 -0400

    All menu animations are done

---
+++ Assets/Scripts/Menu/CreditsScreen.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsScreen : MonoBehaviour {
+    public Animator Anim;
+    bool mopen = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    public void WndowAnimation(bool forward)
+    {
+        if (forward)
+        {
+            if (forward == mopen)
+            {
+                Debug.Log("Stuck");
+                return;
+            }
+
+            Anim.SetBool("show", true);
+            Debug.Log("Open");
+        }
+        else
+        {
+            Anim.SetBool("show", false);
+            Debug.Log("Close");
+        }
+        mopen = forward;
+
+        Debug.Log(mopen);
+    }
+
+    public bool isOpen()
+    {
+        return mopen;
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
