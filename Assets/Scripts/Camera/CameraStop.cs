3ef0669842810284e3f51b0e81d85349e101597f
lago2015 <chilenoo92@gmail.com>
2018-05-24 23:53:08 -0400

    Boss Animations touch up

--- Assets/Scripts/Camera/CameraStop.cs
+++ Assets/Scripts/Camera/CameraStop.cs
@@ -33,6 +33,10 @@ public class CameraStop : MonoBehaviour {
     private Vector3 BossPosition = new Vector3(583, 0, 0);
     private float CameraOffset;
     public Vector3 delta;
+
+    private Vector3 point;
+    private Vector3 delta1;
+    private Vector3 destination;
     private AsteroidSpawner spawnScript;
     private Camera myCamera;
     private bool levelWallActive;
@@ -68,10 +72,9 @@ public class CameraStop : MonoBehaviour {
 	{
         if (target)
         {
-            Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.transform.position);
-
-            Vector3 delta1 = target.transform.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
-            Vector3 destination = transform.position + delta1;
+            point = GetComponent<Camera>().WorldToViewportPoint(target.transform.position);
+            delta1 = target.transform.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+            destination = transform.position + delta1;
             transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
         }
         //Camera fitting to viewport
