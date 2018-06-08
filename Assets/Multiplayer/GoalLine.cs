b300b0ceaeb16e6bc82a4b6a822550957e9b3e42
Douglas1701 <matt.w.doug@gmail.com>
2018-06-07 21:38:37 -0400

    Created Manager and Goaline Scripts

---
+++ Assets/Multiplayer/GoalLine.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalLine : MonoBehaviour
+{
+    public int scoringPlayer;
+    public Transform ballRespawn;
+
+    private MultiplayerManager manager;
+
+	// Use this for initialization
+	void Start ()
+    {
+        manager = GameObject.FindObjectOfType<MultiplayerManager>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        manager.UpdateScore(scoringPlayer, 1);
+        other.transform.position = ballRespawn.transform.position;
+    }
+}
