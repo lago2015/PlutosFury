a996cb718eb631d774c047f0c099e3ea3fcf9f80
AnagramMC <hisham.ata@hotmail.com>
2018-07-17 21:08:18 -0400

    load screen started.

---
+++ Assets/Scripts/Menu/LevelSelect/LoadLevel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour {
+
+    public int loadLevel;
+    public bool isUnlocked = true;
+    private LoadTargetSceneButton loadScreenScript;
+
+	// Use this for initialization
+	void Start ()
+    {
+        loadScreenScript = GameObject.Find("UMenuProManager").GetComponent<LoadTargetSceneButton>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadCurLevel ()
+    {
+        if (isUnlocked)
+        {
+            loadScreenScript.LoadSceneNum(loadLevel);
+        }
+    }
+}
