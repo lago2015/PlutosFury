a7f1bfd54477dacf5d51e1cac651b4df31a99db0
Douglas1701 <matt.w.doug@gmail.com>
2018-07-20 20:40:38 -0400

    Tutorial Triggers and Text functionality Basic

--- Assets/Scripts/HUD/TutorialTip.cs
+++ Assets/Scripts/HUD/TutorialTip.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class TutorialTip : MonoBehaviour
 {
+    [TextArea]
+    public string[] tutorialText;
 
-    Animator anim;
-    bool isDown = false;
+    private Animator anim;
+    private bool isDown = false;
+    private int textIndex = 0;
 
-	// Use this for initialization
-	void Start ()
+    public bool tutorialOn = true;
+
+    // Use this for initialization
+    void Start()
     {
+        // Gets reference to animator and sets the update mode so it will run when gametime is paused
         anim = GetComponent<Animator>();
         anim.updateMode = UnityEngine.AnimatorUpdateMode.UnscaledTime;
+
     }
 	
 	// Update is called once per frame
@@ -29,14 +37,24 @@ public class TutorialTip : MonoBehaviour
 
     public void TipDown()
     {
-        Time.timeScale = 0;
-        anim.SetTrigger("down");
-        isDown = true;
+        // If tutorial is set to on, game will pause and tutorial tip will pop out
+        if (tutorialOn)
+        {
+            Time.timeScale = 0;
+            anim.SetTrigger("down");
+            GetComponentInChildren<Text>().text = tutorialText[textIndex];
+            isDown = true;
+        }
     }
 
     public void TipUp()
     {
+        // Pulls the tutorial tip back up and prepares the next turtorial tip
         anim.SetTrigger("up");
+        if (textIndex < tutorialText.Length)
+        {
+            ++textIndex;
+        }
     }
 
     public void Resume()
@@ -44,6 +62,4 @@ public class TutorialTip : MonoBehaviour
         isDown = false;
         Time.timeScale = 1;
     }
-
-
 }
---
+++ Assets/TutorialTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTrigger : MonoBehaviour
+{
+
+	void OnTriggerEnter(Collider c)
+    {
+        if(c.tag == "Player")
+        {
+            GameObject.FindObjectOfType<TutorialTip>().TipDown();
+
+            Destroy(this.gameObject);
+        }
+    }
+}
