5190cb8311b74d063e83bebadb6ea09dabc5be8c
AnagramMC <hisham.ata@hotmail.com>
2018-07-24 20:01:01 -0400

    pushing level button

--- Assets/Scripts/Menu/LevelSelect/LoadLevel.cs
+++ Assets/Scripts/Menu/LevelSelect/LoadLevel.cs
@@ -1,23 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LoadLevel : MonoBehaviour {
 
     public int loadLevel;
-    public bool isUnlocked = true;
+    public bool isUnlocked;
+
+    public Sprite unlockedSprite;
+    public Sprite lockedSprite;
+
+    public Image menuButton;
+    public Text testText;
     private LoadTargetSceneButton loadScreenScript;
 
 	// Use this for initialization
 	void Start ()
     {
         loadScreenScript = GameObject.Find("UMenuProManager").GetComponent<LoadTargetSceneButton>();
+        if (PlayerPrefs.GetInt(loadLevel + "Unlocked") == 0)
+        {
+            isUnlocked = false;
+        }
+        else
+        {
+            isUnlocked = true;
+        }
+
+
+        if (!isUnlocked)
+        {
+            //lock the menu button
+
+            //menuButton.sprite = lockedSprite;
+            testText.text = "LOCKED";
+        }
+        else
+        {
+            //menuButton.sprite = unlockedSprite;
+            testText.text = (loadLevel - 1).ToString();
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        
 	}
 
     public void LoadCurLevel ()
@@ -26,5 +55,10 @@ public class LoadLevel : MonoBehaviour {
         {
             loadScreenScript.LoadSceneNum(loadLevel);
         }
+    }
+
+    public void SaveUnlockedLevel (int levelBuildNum)
+    {
+        PlayerPrefs.SetInt(levelBuildNum + "Unlocked", 1);
     }
 }
